6d56e2cbb834e8e82d811ff919c359b3dcaf8c8c
Sydney Creel <sydney.creel@outlook.com>
2021-01-27 00:23:33 -0600

    add array of questions in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: "input",
+    name: "title",
+    message: "What is the title of this project?"
+    },
+    {
+    type: "input",
+    name: "description",
+    message: "Give a description of this project."
+    },
+    {
+    type: "input",
+    name: "usage",
+    message: "What is this project used for?"
+    },
+    {
+    type: "list",
+    name: "license",
+    message: "Choose a license for this project:",
+    choices: [
+        "Apache", "Academic", "GNU", "MIT", "Mozilla", "Open"
+    ]
+    },
+    {
+    type: "input",
+    name: "contributing",
+    message: "Who contributed to this project?"
+    },
+    {
+    type: "input",
+    name: "test",
+    message: "Is there a test included?"
+    },
+    {
+    type: "input",
+    name: "questions",
+    message: "What should I do if I have an issue?"
+    },
+    {
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username:"
+    },
+    {
+    type: "input",
+    name: "email",
+    message: "Enter your email:"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# Professional README Generator Starter Code
+# Portfolio Generator
 
-[How to create a Professional README](./readme-guide.md)
+## Description 
+
+This program generates a high-quality, professional README.md
+by prompting the user to input details that correspond to different sections of their REAMDME.md file.
+After the user has completed all prompts, a coherent README.md is displayed.
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+
+## Usage 
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+```md
+![alt text](assets/images/screenshot.png)
+```
+
+
+## Credits
+
+* The starter code for this program can be found here: https://github.com/coding-boot-camp/potential-enigma
+* Sydney Creel
+
+## Features
+
+If your project has a lot of features, consider adding a heading called "Features" and listing them there.
+
+
+## Contributing
+
+https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.txt
